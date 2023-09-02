62b90fc442f62f79d5e03f1bd6f0a015b1db91c5
Muhammad Ali <malishoaib.2001@gmail.com>
2023-09-02 22:16:38 +0500

    Screens added

---
+++ src/screens/home.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>componentName</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {}
+});
